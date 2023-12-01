dccb1255f8f272d133b051da71e50c4eb5d7ea3c
58089 <58089@etu.he2b.be>
2023-12-02 00:45:12 +0100

    Day 1, part 1 : added comments

--- Day1/Day1_1/Program.cs
+++ Day1/Day1_1/Program.cs
@@ -1,34 +1,25 @@
-﻿namespace Day1_1
-{
-    class Program
-    {
-        // I'm literally just starting out .NET and C# for the first time
-        static void Main(string[] args)
-        {
-            var lines = File.ReadLines("../input");
+﻿// I'm literally just starting out .NET and C# for the first time
 
-            int total = 0;
+var lines = File.ReadLines("../input");
 
-            foreach (string line in lines)
-            {
-                int lo = 0;
-                int hi = line.Length - 1;
-                int a = -1, b = -1;
+int total = 0;
 
-                try {
-                    while (!Char.IsDigit(line[lo])) lo++;
-                    a = line[lo] - '0';
-                    while (!Char.IsDigit(line[hi])) hi--;
-                    b = line[hi] - '0';
-                } catch (IndexOutOfRangeException) {
-                    Console.Error.WriteLine("ligne fautive : " + line);
-                }
+foreach (string line in lines)
+{ // this is in O(2n) * m where n = lineSize and m = number of lines
+    int lo = 0;
+    int hi = line.Length - 1;
+    int a = -1, b = -1;
 
-                total += a * 10 + b;
-            }
+    // find the first digit
+    while (!Char.IsDigit(line[lo])) lo++;
+    a = line[lo] - '0'; // funny char manipulation
 
-            Console.WriteLine(total);
+    // find the last digit
+    while (!Char.IsDigit(line[hi])) hi--;
+    b = line[hi] - '0';
 
-        }
-    }
+    // finding out the number
+    total += a * 10 + b;
 }
+
+Console.WriteLine(total);

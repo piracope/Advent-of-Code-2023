3a22464b05e211ad19b03647fd6781b6729b85bb
58089 <58089@etu.he2b.be>
2023-12-02 00:41:47 +0100

    Day 1, part 2 : added comments

--- Day1/Day1_2/Program.cs
+++ Day1/Day1_2/Program.cs
@@ -1,19 +1,37 @@
 ﻿var lines = File.ReadLines("../input");
+
 int total = 0;
-foreach (string line in lines) {
+foreach (string line in lines)
+{
+    // only ten possible digits to find, no need to do a map
+    // will store the lowest index of each digit
     int[] lowestIdx = Enumerable.Repeat(int.MaxValue, 10).ToArray();
     int[] highestIdx = Enumerable.Repeat(-1, 10).ToArray();
+
+    // just a translation map
     string[] digitToStr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-    for (int i = 0; i < 10; i++) {
+
+    // we'll go look for each digit.
+    for (int i = 0; i < 10; i++)
+    {
+        // get the first appearance of "i" both as a digit and as a string
         int lowestDig = line.Contains(i.ToString()) ? line.IndexOf(i.ToString()) : int.MaxValue;
         int lowestStr = line.Contains(digitToStr[i]) ? line.IndexOf(digitToStr[i]) : int.MaxValue;
+
+        // then its latest appearance
         int highestDig = line.Contains(i.ToString()) ? line.LastIndexOf(i.ToString()) : -1;
         int highestStr = line.Contains(digitToStr[i]) ? line.LastIndexOf(digitToStr[i]) : -1;
-        lowestIdx[i] = Math.Min(Math.Min(lowestIdx[i], lowestDig), lowestStr);
-        highestIdx[i] = Math.Max(Math.Max(highestIdx[i], highestDig), highestStr);
+
+        // and then we set in our array whichever is the low/highest, the digit or the string
+        lowestIdx[i] = Math.Min(lowestDig, lowestStr);
+        highestIdx[i] = Math.Max(highestDig, highestStr);
     }
+
+    // we find which digit appeared first/last
     int a = Array.IndexOf(lowestIdx, lowestIdx.Min());
     int b = Array.IndexOf(highestIdx, highestIdx.Max());
+
+    // and we compute the actual number
     total += a * 10 + b;
 }
 Console.WriteLine(total);

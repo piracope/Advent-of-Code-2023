3fc076b4208eb87514af4cdf8355cd767b6a0bf3
58089 <58089@etu.he2b.be>
2023-12-02 14:46:44 +0100

    Day 2, part 1

---
+++ Day2/Day2_1/Program.cs
@@ -0,0 +1,41 @@
+﻿var lines = File.ReadAllLines("../input");
+
+var MAX_VALUES = new Dictionary<string, int> {
+    {"red", 12},
+    {"green", 13},
+    {"blue", 14}
+};
+
+// triangular number : n + n-1 + n-2 + ... + 1
+int total = lines.Length * (lines.Length + 1) / 2;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    // this is gonna be a umm... i forgot the name of this type of algorithms
+    bool dropped = false;
+
+    // Game 1: [.....; .......; .....; ......]
+    // => ([x ..., y ...], [x ..., y ...])
+    var drops = lines[i].Split(':')[1].Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+    for (int j = 0; !dropped && j < drops.Length; j++)
+    {
+        // => [x ...], [y ...]
+        var colors = drops[j].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        for (int k = 0; !dropped && k < colors.Length; k++)
+        {
+            // => ([x], [...])
+            var amountAndColor = colors[k].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if (MAX_VALUES[amountAndColor[1]] < int.Parse(amountAndColor[0]))
+            {
+                // we can just skip to the next line
+                dropped = true;
+
+                // instead of adding the IDs that are good, we substract the IDs that aren't
+                total -= i + 1;
+            }
+        }
+    }
+}
+
+Console.WriteLine(total);
